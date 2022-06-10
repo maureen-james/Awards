c89ea7f904d19ed06ac030657bf24d87000277c5
maureen-james <manjax97@gmail.com>
2022-06-11 01:47:35 +0300

    add and register bootstrap to installed apps

--- awaards/settings.py
+++ awaards/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'awards',
+    'bootstrap4',
+    
 ]
 
 MIDDLEWARE = [
--- awards/urls.py
+++ awards/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 
 
 urlpatterns=[
-     path('',views.welcome,name = 'welcome'),
+    path('',views.welcome,name = 'welcome'),
 ]
