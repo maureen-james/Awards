a463a4c41cef4969c318aab0d7fa7d3c7e2005f5
maureen-james <manjax97@gmail.com>
2022-06-12 02:58:17 +0300

    create profile model

--- awards/models.py
+++ awards/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Project(models.Model):
@@ -8,10 +10,73 @@ class Project(models.Model):
     This class takes care of the posted projects
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
-
     image = CloudinaryField('image')
     title = models.CharField(max_length=50)
     description=models.CharField(max_length=100)
     url = models.TextField()
     posted_date = models.DateTimeField(auto_now_add=True)
     rate = models.IntegerField(default=0)
+
+    @classmethod
+    def get_project_by_user(cls, user):
+        project = cls.objects.filter(user=user)
+        return project
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    #  get by id
+    @classmethod
+    def get_one_project(cls, id):
+        project = cls.objects.get(id=id)
+        return project
+
+    
+    @classmethod
+    def search_by_title(self, search_title):
+        
+        projects = Project.objects.filter(title__icontains=search_title)
+        return projects
+  
+
+    def __str__(self):
+        return self.user.username   
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    profile_photo = CloudinaryField('image')
+
+    bio = models.TextField(max_length=500, blank=True, null=True)
+
+    contact = models.CharField(max_length=50, blank=True, null=True)
+
+    def update(self):
+        self.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profile_by_user(cls, user):
+        profile = cls.objects.filter(user=user)
+        return profile       
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+    @classmethod
+    def search_profiles(cls, search_term):
+        profiles = cls.objects.filter(user__username__icontains=search_term).all()
+        return profiles            
+
