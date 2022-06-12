cfabda3aad529b3a1d49223d0311849bc18d6cf6
maureen-james <manjax97@gmail.com>
2022-06-12 23:32:29 +0300

    add profile serializer

---
+++ awards/serializer.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Project,Profile
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('profile_photo', 'bio', 'user')
