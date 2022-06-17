d7fc21c39fb9ac0912662bdcb807823b2c424ec0
maureen-james <manjax97@gmail.com>
2022-06-17 11:40:32 +0300

    deployment to heroku

--- awaards/settings.py
+++ awaards/settings.py
@@ -35,9 +35,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-xg0!^ab6e=#(0=oh)82wzeoy#xo^3hyj!o=)!b62cekce=_4-1'
+SECRET_KEY = 'django-insecure-xg0!^ab6e=#(0=oh)82wzeoy#xo^3hyj!o=)!b62cekce=_4-1'
 MODE=config("MODE", default="dev")
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
